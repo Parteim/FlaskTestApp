8b93263ce3963760c754332a0a1d6f60318178e2
Parteim <Sergej_kajnov@mail.ru>
2020-03-18 07:19:59 +0700

    add features of search and make form of creation posts

--- src/posts/blueprint.py
+++ src/posts/blueprint.py
@@ -1,18 +1,38 @@
-from flask import Blueprint
-from flask import render_template
-
-from models import Post
+from flask import Blueprint, request, render_template
+from .forms import PostForm
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
 @posts.route('/')
 def posts_page():
-    posts = Post.query.all()
+    search_value = request.args.get('search', '')
+
+    if search_value:
+        posts = Post.query.filter(
+            Post.title.contains(search_value) |
+            Post.body.contains(search_value)
+        ).all()
+    else:
+        posts = Post.query.all()
     return render_template('posts/posts.html', title='Posts', posts=posts)
 
 
 @posts.route('/<slug>')
 def post_detail(slug):
-    post = Post.query.filter(Post.slug==slug).first()
-    return render_template('posts/post.html', title='Posts', post=post)
+    post = Post.query.filter(Post.slug == slug).first()
+    return render_template('posts/post.html', title=post.title, post=post)
+
+
+@posts.route('/tag/<slug>')
+def posts_by_tag(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/posts.html', title=tag.name, posts=posts, tag=tag)
+
+
+@posts.route('/create_post')
+def create_post():
+    form = PostForm()
+    return render_template('posts/create_post.html', title='Create post', form=form)
